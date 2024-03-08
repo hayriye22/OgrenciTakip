631b9c668f2afca2d7b89e2b76f62fdcc129e7f1
hayriye466 <dhayriye466@gmail.com>
2024-03-08 10:51:02 +0300

    Proje dosyası ekle.

---
+++ OgrenciTakip/Controllers/OgrenciController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OgrenciTakip.Models;
+
+namespace OgrenciTakip.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        static List<OgrenciModel> ogrenciler = new List<OgrenciModel>();
+        
+        public IActionResult Index()
+        {      
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Kayit(OgrenciModel ogrenci)
+        {
+            if (ogrenci.OgrenciNo < 1)
+            {
+                return Content(" Öğrenci numarası geçerli değil.");
+            }
+            else if (string.IsNullOrEmpty(ogrenci.Adi))
+            {
+                return Content("ögrenci adı geçerli değil");
+            }
+            else if (string.IsNullOrEmpty(ogrenci.SoyAdi))
+            {
+                return Content("ogrenci soyadı geçerli değil");
+            }
+            else if (string.IsNullOrEmpty(ogrenci.Sinifi))
+            {
+                return Content("ogrenci sınıfını giriniz lütfen");
+            }
+            else
+            {
+                ogrenciler.Add(ogrenci);
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpGet]
+        public IActionResult Listele(OgrenciModel ogrenci)
+        {
+            return View(ogrenciler);
+        }
+    }
+}
---
+++ OgrenciTakip/Models/OgrenciModel.cs
@@ -0,0 +1,12 @@
+﻿namespace OgrenciTakip.Models
+{
+    public class OgrenciModel
+    {
+        public string? Adi { get; set; }
+        public string? SoyAdi{ get; set; }
+        public int OgrenciNo { get; set; }
+        public string? Sinifi { get; set; }
+      
+
+    }
+}
